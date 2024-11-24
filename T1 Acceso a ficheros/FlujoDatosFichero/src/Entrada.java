173f3c7ec8cee7c9135af7ceba9f1d08ddab9ac9
ArandaLidia <arandagomezlidia@gmail.com>
2024-11-24 13:23:48 +0100

    up

---
+++ T1 Acceso a ficheros/FlujoDatosFichero/src/Entrada.java
@@ -0,0 +1,8 @@
+import model.Usuario;
+
+public class Entrada {
+    public static void main(String[] args) {
+        Usuario usuario =new Usuario();
+
+    }
+}
---
+++ T1 Acceso a ficheros/FlujoDatosFichero/src/model/Usuario.java
@@ -0,0 +1,47 @@
+package model;
+
+public class Usuario {
+    private String nombre, apellidos, correo, telefono;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nombre, String apellidos, String correo, String telefono) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.correo = correo;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
